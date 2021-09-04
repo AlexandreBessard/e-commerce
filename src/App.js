26ac88a2a9eb89c58a59f80c50343c1621c34cdc
AlexandreBessard <alexandre.bessard@gmail.com>
2021-09-04 17:34:56 +0200

    added sass dependencies and homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
+import HomePage from './homepage.component';
 
 function App() {
   return (
-    <p>Hello World</p>
+    <HomePage />
   );
 }
 
